48161774c85c57e71ec7f6731054f6313b85818b
francesc.gabriel.marti <200187Secret2001>
2022-10-15 20:49:15 +0200

    big update

---
+++ puItem.js
@@ -0,0 +1,30 @@
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+AWS.config.update({
+    region: process.env.AWS_DEFAULT_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+     
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName: 'Usuaris',
+    Item: {
+      'Id' : {N: '003'},
+      'User_email' : {S: 'francisco@gmail.com'},
+      'CP' : {N: '08021'},
+      'Password' : {S: 'Thisismypass'},
+      }
+  };
+  
+  // Call DynamoDB to add the item to the table
+  dynamodb.putItem(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
+  
