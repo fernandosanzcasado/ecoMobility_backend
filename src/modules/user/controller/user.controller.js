b1db6fc380271f3af65cfc2fa53dae15c7b7dffd
ElSeKo420 <marc.fonseca@estudiantat.upc.edu>
2022-10-23 13:24:11 +0200

    CRUD  Users

--- src/modules/user/controller/user.controller.js
+++ src/modules/user/controller/user.controller.js
@@ -1,16 +1,29 @@
 const userService = require("../service/user.service");
 
 //fitxer que s'encarrega de gestiona les request i responses dels usuaris
-class userController {
-  async findById(req, res) {
-    const data = await userService.findById(req.params.Id);
-    res.json(data);
-  }
+class userController{
 
-  async create(req, res) {
-    const data = await userService.create(req.body);
-    res.json(data);
-  }
+    async findByEmail(req, res) {
+        const data = await userService.findByEmail(req.params.email);
+        res.json(data)
+    }
+
+    async create(req,res){
+        const data = await userService.create(req.body);
+        res.json(data);
+    }
+
+    async updateUserInfo(req,res){
+        //console.log(req.params.email + req.params.body);
+        const data = await userService.updateUserInfo(req.params.email, req.body);
+        res.json(data);
+
+    }
+
+    async deleteByEmail(req,res){
+        await userService.deleteByEmail(req.params.email);
+        res.json('User deleted successfully');
+    }
 }
 
 module.exports = new userController();
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,9 @@
 const userController = require("../modules/user/controller/user.controller");
 
-module.exports = async (app) => {
-  app.get(`/api/v1/users/:Id`, userController.findById);
-  app.post(`/api/v1/users`, userController.create);
+
+module.exports = async(app) =>{
+    app.get(`/api/users/:email`, userController.findByEmail);
+    app.post(`/api/users`, userController.create);
+    app.put(`/api/users/:email`, userController.updateUserInfo);
+    app.delete(`/api/users/:email`, userController.deleteByEmail);
 };
