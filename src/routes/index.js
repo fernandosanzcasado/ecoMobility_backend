27d6bdda0b4dc18a2b8e39fa7265102c2a51a7a6
fernandosanzcasado <fernandosanzeleven@gmail.com>
2022-10-25 19:16:08 +0200

    /api/v1/ generic beggining of API path

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
+const express = require("express");
+
 const usersRouter = require("./user.router");
 const estacionesRouter = require("./estaciones.router");
 
 function routerApi(app) {
-  app.use("/api/users", usersRouter);
-  app.use("/api/v1/estaciones", estacionesRouter);
+  const router = express.Router();
+  app.use("/api/v1", router);
+  router.use("/users", usersRouter);
+  router.use("/estaciones", estacionesRouter);
 }
 
 module.exports = routerApi;
