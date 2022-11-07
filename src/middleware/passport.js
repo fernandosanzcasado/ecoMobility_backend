d73bbb87f91fa273f5ebd6d09b9d40729b5b538a
ElSeKo420 <marc.fonseca@estudiantat.upc.edu>
2022-11-07 17:23:04 +0100

    login with passport

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -1,14 +1,35 @@
-
-/*
-const passport = require('passport');
-const passportLocal = require('passport-local').Strategy;
-const 
+const { authenticate } = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrpyt = require('bcrypt')
 
 
-passport.use(new passportLocal(function(email, passport, done){
 
-})
 
-}))
 
-*/
+function initialize(passport, findUserByEmail){
+    const authenticateUser = async (email, password, done) => {
+        const user = findUserByEmail(email);
+
+        if(user == null){
+            return done(null,false, {message: 'User not found'})
+        }
+
+
+        try{
+            if(await bcrpyt.compare(password, user.password)){
+                return done(null, user)
+            }else{
+                return done(null, false, {message: 'Password incorrect.'})
+            }
+        }catch(err){
+            return done(err)
+        }
+    }
+    passport.use(new LocalStrategy({usernameField: 'email'}, 
+    authenticateUser))
+    passport.serializeUser((user, done) =>{  })
+    passport.deserializeUser((id, done) =>{  })
+}
+
+
+module.exports = initialize
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const passport = require("passport")
+const session = require("express-session")
 
 const { check, validationResult } = require("express-validator");
 
 const userController = require("../modules/user/controller/user.controller");
-const validateRequsestSchema = require('../middleware/validateRequestSchema');
 const registerSchema = require('../schemas/registerSchema');
 const loginSchema = require('../schemas/loginSchema');
+const validateRequsestSchema = require('../middleware/validateRequestSchema');
+
+const initializePassport = require('../middleware/passport');
+const { application } = require("express");
+initializePassport(passport, 
+    email =>
+    userController.findByEmail(email)
+)
 
 const router = express.Router();
+
+router.use(session({
+    secret: 'YUNG_BEEF',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+router.use(passport.initialize())
+router.use(passport.session())
 
 router.post(`/`, userController.create);
 
@@ -15,7 +33,7 @@ router.get(`/:email`, userController.findByEmail);
 router.put(`/:email`, userController.updateUserInfo);
 router.delete(`/:email`, userController.deleteByEmail);
 
-router.post(`/login`,loginSchema, validateRequsestSchema, userController.loginUser);
+router.post(`/login`,loginSchema, validateRequsestSchema, passport.authenticate('local'));
 router.post(`/register`,registerSchema,validateRequsestSchema,userController.registerUser);
 
 module.exports = router;
