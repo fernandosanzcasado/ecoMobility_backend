64e7d8951237f8d6ae8e16808acdfc8ebb2603cc
Marc <marc.fonseca@estudiantat.upc.edu>
2022-12-06 02:35:01 +0100

    custom validation errors[#176]

--- src/middleware/validateRequestSchema.js
+++ src/middleware/validateRequestSchema.js
@@ -1,8 +1,21 @@
 const { validationResult } = require("express-validator");
 
 
+
+const myValidationResult = validationResult.withDefaults({
+    formatter: error => {
+      return {
+        name : 'Invalid ' + error.param,
+        message: error.msg,
+        status: 400
+      };
+    },
+  });
+
+
+
 function validateRequsestSchema(req,res,next){
-    const errors = validationResult(req);
+    const errors = myValidationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
@@ -10,4 +23,3 @@ function validateRequsestSchema(req,res,next){
 }
 
 module.exports = validateRequsestSchema;
-
