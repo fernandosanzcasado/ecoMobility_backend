80c872cfd15dcdf08ecbb56ee52d96e5c6123bcd
XavierPina <xavier.pina@estudiantat.upc.edu>
2022-12-14 17:31:33 +0100

    [#216] Creación socket servidor

---
+++ src/socket/serverSocket.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = 3000;
+
+io.on("connection", (socket) => {
+  console.log("Servidor conectado");
+});
+
+server.listen(port, () => console.log("server running on port " + port));
