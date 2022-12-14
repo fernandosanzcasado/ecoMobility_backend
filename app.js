c6285c660719938dc244a65c8b53d2a9a973308d
Fernando Sanz <fernandosanzeleven@gmail.com>
2022-12-14 16:07:11 +0100

    Deleted server.js

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const server = require(`./server`);
+const express = require("express");
+const app = express();
+const port = 3000;
 
+const bodyParser = require("body-parser");
 require(`dotenv`).config();
-server.createServer();
+
+// const routerApi = require("./src/routes"); // ESTO NO SE PUEDE HACER TAL COMO ESTA ESTRUCTURADO EL PROYECTO (SPRINT 2 LO MIRAMOS)
+
+app.use(bodyParser.json());
+
+// routes
+require(`./src/routes/`)(app);
+// routerApi(app); // ESTO NO SE PUEDE HACER TAL COMO ESTA ESTRUCTURADO EL PROYECTO (SPRINT 2 LO MIRAMOS)
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
