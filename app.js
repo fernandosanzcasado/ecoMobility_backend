b8445ae0c3db9fb066f6b219bfd4c1d61f7897e6
Fernando Sanz <fernandosanzeleven@gmail.com>
2022-12-14 16:19:46 +0100

    Clear app.js

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ const port = 3000;
 const bodyParser = require("body-parser");
 require(`dotenv`).config();
 
-// const routerApi = require("./src/routes"); // ESTO NO SE PUEDE HACER TAL COMO ESTA ESTRUCTURADO EL PROYECTO (SPRINT 2 LO MIRAMOS)
+const routerApi = require("./src/routes");
 
 app.use(bodyParser.json());
 
-// routes
-require(`./src/routes/`)(app);
-// routerApi(app); // ESTO NO SE PUEDE HACER TAL COMO ESTA ESTRUCTURADO EL PROYECTO (SPRINT 2 LO MIRAMOS)
+routerApi(app);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
