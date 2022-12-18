3ed29c60a33076ca09255eafa550ff9972b4e075
fernandosanzcasado <fernandosanzeleven@gmail.com>
2022-12-19 00:27:32 +0100

    Script seems to be working

--- server.sh
+++ server.sh
@@ -9,15 +9,22 @@ procesos=$(lsof -t -i :3000)
 
 # Si hay algún proceso en la lista, matarlos
 if [ -n "$procesos" ]; then
-  xargs kill -9 <<< "$procesos"
+  kill -9 "$procesos"
 fi
 
 # Crear la carpeta 'logs' si no existe
 mkdir -p logs
 
 # Eliminar todos los archivos de log que no sean los 10 más recientes
-find logs -name "server*.log" -type f -printf "%AT %p\n" | sort -r -k1.1n | tail -n +11
-find logs -name "error*.log" -type f -printf "%AT %p\n" | sort -r -k1.1n | tail -n +11
+serverLogs=$(find logs -name "server*.log" -type f -printf "%T@ %p\n" | sort -k1,1nr | cut -d " " -f2 | tail -n +11)
+if [ -n "$serverLogs" ]; then
+  rm $serverLogs
+fi
+
+errorLogs=$(find logs -name "error*.log" -type f -printf "%T@ %p\n" | sort -k1,1nr | cut -d " " -f2 | tail -n +11)
+if [ -n "$errorLogs" ]; then
+  rm $errorLogs
+fi
 
 # Ejecutar el comando 'npm start' y redirigir el output a la carpeta 'logs' con el nombre de la fecha y hora actuales
 npm start >>logs/"server_$fecha_hora".log 2>>logs/"error_$fecha_hora".log
